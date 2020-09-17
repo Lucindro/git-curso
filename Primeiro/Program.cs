289afb3fc49ceb5542e7330556895274c2b7fc3a
Bruno Lucindro <brunolucindro@hotmail.com>
2020-09-17 19:12:21 -0300

    Projeto realizando cadastro de conta, saque e deposito

---
+++ Primeiro/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transacao t = new Transacao();
+            Console.WriteLine("informe seu nome:");
+            string nome= Console.ReadLine();
+
+            Console.WriteLine("Conta:");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Saldo inicial?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp=='s')
+            {
+                Console.WriteLine("Qual o valor");
+                float saldo = float.Parse(Console.ReadLine());
+                t = new Transacao(nome, conta, saldo);
+            }
+            else
+            {
+                t = new Transacao(nome, conta);
+            }
+            
+            
+            Console.WriteLine("Dados da conta");
+            Console.WriteLine("Conta: " +
+                t.Conta+
+                ", Titular: "+
+                t.Nome+
+                ", Saldo: $"+
+                t.Saldo);
+
+
+            Console.WriteLine("Entre com um valor de deposito:");
+            float dep = float.Parse(Console.ReadLine());
+            t.Deposito(dep);
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine("Conta: " +
+                t.Conta +
+                ", Titular: " +
+                t.Nome +
+                ", Saldo: $" +
+                t.Saldo);
+
+            Console.WriteLine("Entre com um valor de saque:");
+            float saq = float.Parse(Console.ReadLine());
+             t.Saque(saq);
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine("Conta: " +
+                t.Conta +
+                ", Titular: " +
+                t.Nome +
+                ", Saldo: $" +
+               t.Saldo);
+
+        }
+    }
+}
---
+++ Primeiro/Transacao.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Primeiro
+{
+    class Transacao
+    {
+        private string _nome;
+        public int Conta { get; private set; }
+        public float Saldo { get; private set; }
+
+        public Transacao()
+        {
+        }
+
+        public Transacao(string nome, int conta)
+        {
+            _nome = nome;
+            this.Conta = conta;
+            Saldo = 0;
+        }
+
+        public Transacao(string nome, int conta, float saldo) : this(nome, conta)
+        {
+            Saldo = saldo;
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set { 
+                if(value!=null || value.Length > 1) { _nome = value; }
+            }
+        }
+
+
+        /* OUTROS METODOS*/
+        public void Deposito(float dep)
+        {
+            
+            Saldo= Saldo+ dep;
+            
+        }
+        public void Saque(float saq)
+        {
+            
+            Saldo= Saldo - (saq+5);
+            
+        }
+    }
+     
+}
